8c5d0e337f5544c373dfad3534346ab9a4a53adf
idotsenko <idotsenko@usergate.com>
2025-08-08 06:42:53 +0300

    added another exceptions

--- game.py
+++ game.py
@@ -1,14 +1,56 @@
 # game.py
 
 from gameparts import Board
+from gameparts.exceptions import FieldIndexError
 
 def main():
     game = Board()
     game.display()
-    # Тут пользователь вводит координаты ячейки.
-    row = int(input('Введите номер строки: '))
-    column = int(input('Введите номер столбца: '))
-    # В метод make_move передаются те координаты, которые ввёл пользователь.
+
+    # Запускается бесконечный цикл.
+    while True:
+        # В этом блоке содержатся операции, которые могут вызвать исключение.
+        try:
+            # Пользователь вводит значение номера строки.
+            row = int(input('Введите номер строки: '))
+            # Если введённое число меньше 0 или больше
+            # или равно game.field_size...
+            if row < 0 or row >= game.field_size:
+                # ...выбрасывается собственное исключение FieldIndexError.
+                raise FieldIndexError
+            column = int(input('Введите номер столбца: '))
+            # Если введённое число меньше 0 или больше
+            # или равно game.field_size...
+            if column < 0 or column >= game.field_size:
+                # ...выбрасывается собственное исключение FieldIndexError.
+                raise FieldIndexError
+        except ValueError:
+            print(
+                'Буквы вводить нельзя. Только числа.\n'
+                'Пожалуйста, введите значения для строки и столбца заново.'
+            )
+            continue
+
+        # Если возникает исключение FieldIndexError...
+        except FieldIndexError:
+            # ...выводятся сообщения...
+            print(
+                'Значение должно быть неотрицательным и меньше '
+                f'{game.field_size}.'
+            )
+            print('Пожалуйста, введите значения для строки и столбца заново.')
+            # ...и цикл начинает свою работу сначала,
+            # предоставляя пользователю ещё одну попытку ввести данные.
+            continue
+        except Exception as e:
+            print(f'Возникла ошибка: {e}')
+        # Если в блоке try исключения не возникло...
+        else:
+            # ...значит, введённые значения прошли все проверки
+            # и могут быть использованы в дальнейшем.
+            # Цикл прерывается.
+            break
+
     game.make_move(row, column, 'X')
     print('Ход сделан!')
     game.display()
--- gameparts/exceptions.py
+++ gameparts/exceptions.py
@@ -1,3 +1,11 @@
+# gameparts/exceptions.py
+
 class FieldIndexError(IndexError):
     """Выбрасывается, если выбрано значение вне поля."""
-    
+
+    def __init__(
+        self,
+        # Текст по умолчанию.
+        message='Введено значение за границами игрового поля!'  
+    ):
+        super().__init__(message) 
