a0a04b25bd3d9b393d6d4bafaf5621e17956912d
idotsenko <idotsenko@usergate.com>
2025-08-08 06:31:43 +0300

    moved the code to separate modules

--- game.py
+++ game.py
@@ -1,26 +1,17 @@
-# Объявить класс.
-class Board:
-    # Инициализировать игровое поле - список списков с пробелами.
-    # Пробелы - это пустые клетки.
-    def __init__(self):
-        self.board = [[' ' for _ in range(3)] for _ in range(3)]
+# game.py
 
-    # Метод, который обрабатывает ходы игроков.
-    def make_move(self, row, col, player):
-        self.board[row][col] = player
+from gameparts import Board
 
-    # Метод, который отрисовывает игровое поле.
-    def display(self):
-        for row in self.board:
-            print('|'.join(row))
-            print('-' * 5)
+def main():
+    game = Board()
+    game.display()
+    # Тут пользователь вводит координаты ячейки.
+    row = int(input('Введите номер строки: '))
+    column = int(input('Введите номер столбца: '))
+    # В метод make_move передаются те координаты, которые ввёл пользователь.
+    game.make_move(row, column, 'X')
+    print('Ход сделан!')
+    game.display()
 
-# Создать игровое поле - объект класса Board.
-game = Board()
-# Отрисовать поле в терминале.
-game.display()
-# Разместить на поле символ по указанным координатам - сделать ход.
-game.make_move(1, 1, 'X')
-print('Ход сделан!')
-# Перерисовать поле с учётом сделанного хода.
-game.display()
+if __name__ == '__main__':
+    main()
