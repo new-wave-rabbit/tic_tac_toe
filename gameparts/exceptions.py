73315f981742a6ab5e8b743bf71bb1c42add11a6
idotsenko <idotsenko@usergate.com>
2025-08-08 06:48:35 +0300

    added CellOccupiedError

--- gameparts/exceptions.py
+++ gameparts/exceptions.py
@@ -1,11 +1,20 @@
 # gameparts/exceptions.py
 
 class FieldIndexError(IndexError):
-    """Выбрасывается, если выбрано значение вне поля."""
+    """ Выбрасывается, если выбрано значение вне поля. """
 
     def __init__(
-        self,
-        # Текст по умолчанию.
-        message='Введено значение за границами игрового поля!'  
-    ):
-        super().__init__(message) 
+            self,
+            message='Введено значение за границами игрового поля!'
+        ):
+        
+        super().__init__(message)
+
+# Вот оно - новое исключение, унаследованное от базового класса Exception.
+class CellOccupiedError(Exception):
+    def __init__(
+            self,
+            message='Попытка изменить занятую ячейку'
+        ):
+        
+        super().__init__(message)
