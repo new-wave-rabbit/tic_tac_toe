d6fa39dce219d9124445f307c631b1c3223d1418
idotsenko <idotsenko@usergate.com>
2025-08-08 06:52:43 +0300

    added is_board_full method

--- gameparts/parts.py
+++ gameparts/parts.py
@@ -23,3 +23,15 @@ class Board:
             'Объект игрового поля размером '
             f'{self.field_size}x{self.field_size}'
         )
+    
+    def is_board_full(self):
+        # Цикл проходится по всем столбцам игрового поля.
+        for i in range(self.field_size):
+            # А потом по всем строчкам.
+            for j in range(self.field_size):
+                # Если находит свободную ячейку...
+                if self.board[i][j] == ' ':
+                    # ...игра продолжается.
+                    return False
+        # Иначе - ничья!
+        return True
