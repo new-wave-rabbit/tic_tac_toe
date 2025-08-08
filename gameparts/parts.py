3086c3f786d66f0a6ce880dbefac02dff0eed428
idotsenko <idotsenko@usergate.com>
2025-08-08 06:30:18 +0300

    added parts.py

---
+++ gameparts/parts.py
@@ -0,0 +1,25 @@
+# gameparts/parts.py
+
+class Board:
+    """Класс, который описывает игровое поле."""
+
+    field_size = 3
+
+    def __init__(self):
+        self.board = [
+            [' ' for _ in range(self.field_size)] for _ in range(self.field_size)
+        ]
+
+    def make_move(self, row, col, player):
+        self.board[row][col] = player
+
+    def display(self):
+        for row in self.board:
+            print('|'.join(row))
+            print('-' * 5)
+
+    def __str__(self):
+        return (
+            'Объект игрового поля размером '
+            f'{self.field_size}x{self.field_size}'
+        )
